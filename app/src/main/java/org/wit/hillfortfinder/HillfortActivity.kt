9cd6871afc3dc2a1dc817ccc3654cd4bd7ecf897
Richard Whitney <rwhitney32@gmail.com>
2019-10-18 19:40:12 +0100

    basic event handling for add hillfort button

--- app/src/main/java/org/wit/hillfortfinder/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfortfinder/HillfortActivity.kt
@@ -2,14 +2,26 @@ package org.wit.hillfortfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        info("Hillfort Activity Started...")
         setContentView(R.layout.activity_hillfort)
+        info("Hillfort Activity Started...")
+
+        btnAdd.setOnClickListener {
+            val hillforTitle = hillfortTitle.text.toString()
+            if (hillforTitle.isNotEmpty()) {
+                info("Add Button Pressed: $hillforTitle")
+            }
+            else {
+                toast("Please Enter A Title")
+            }
+        }
     }
 }
