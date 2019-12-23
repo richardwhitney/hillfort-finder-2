bc2d80578452c26e9c13048d899b455c251ec3ab
Richard Whitney <rwhitney32@gmail.com>
2019-12-23 15:54:38 +0000

    refactored MapActivity to use MVP

--- app/src/main/java/org/wit/hillfortfinder/activities/MapActivity.kt
+++ app/src/main/java/org/wit/hillfortfinder/activities/MapActivity.kt
@@ -1,77 +1,44 @@
 package org.wit.hillfortfinder.activities
 
-import android.app.Activity
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-import com.google.android.gms.maps.CameraUpdateFactory
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.hillfortfinder.R
-import org.wit.hillfortfinder.models.Location
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerDragListener, GoogleMap.OnMarkerClickListener {
+class MapActivity : AppCompatActivity(), GoogleMap.OnMarkerDragListener, GoogleMap.OnMarkerClickListener {
 
-    private lateinit var map: GoogleMap
-    var location = Location()
+    lateinit var map: GoogleMap
+    lateinit var presenter: MapsPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-        location = intent?.extras?.getParcelable<Location>("location")!!
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        presenter = MapsPresenter(this)
+        mapFragment.getMapAsync {
+            map = it
+            map.setOnMarkerDragListener(this)
+            map.setOnMarkerClickListener(this)
+            presenter.initMap(map)
+        }
     }
 
-    override fun onMarkerDragStart(p0: Marker) {
+    override fun onMarkerDragStart(marker: Marker) {}
 
-    }
+    override fun onMarkerDrag(marker: Marker) {}
 
-    override fun onMarkerDrag(p0: Marker) {
-
-    }
-
-    override fun onMarkerDragEnd(p0: Marker) {
-        location.lat = p0.position.latitude
-        location.lng = p0.position.longitude
-        location.zoom = map.cameraPosition.zoom
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val loc = LatLng(location.lat, location.lng)
-        val options = MarkerOptions()
-            .title("Hillfort")
-            .snippet("GPS: " + loc.toString())
-            .draggable(true)
-            .position(loc)
-        map.addMarker(options)
-        map.setOnMarkerDragListener(this)
-        map.setOnMarkerClickListener(this)
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, location.zoom))
+    override fun onMarkerDragEnd(marker: Marker) {
+        presenter.doUpdateLocation(marker.position.latitude, marker.position.longitude, map.cameraPosition.zoom)
     }
 
     override fun onBackPressed() {
-        val resultIntent = Intent()
-        resultIntent.putExtra("location", location)
-        setResult(Activity.RESULT_OK, resultIntent)
-        finish()
-        super.onBackPressed()
+        presenter.doOnBackPressed()
     }
 
-    override fun onMarkerClick(p0: Marker): Boolean {
-        val loc = LatLng(location.lat, location.lng)
-        p0.snippet = "GPS: ${loc.toString()}"
+    override fun onMarkerClick(marker: Marker): Boolean {
+        presenter.doUpdateMarker(marker)
         return false
     }
 }
-
